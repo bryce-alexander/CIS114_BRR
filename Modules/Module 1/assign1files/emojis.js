9b379f3a76afb98842e4a4198bee738145418bcd
Bryce <bryce.rankin@gmail.com>
2020-08-30 21:19:52 -0700

    Assignment 1 Fixed & Complete

--- Modules/Module 1/assign1files/emojis.js
+++ Modules/Module 1/assign1files/emojis.js
@@ -1,20 +1,19 @@
 'use strict'
  
 let objectArray = [
-    {"Name": "Name1", "Age": 1, "Emoji": 🥇},
-    {"Name": "Name2", "Age": 2, "Emoji": 🥈},
-    {"Name": "Name3", "Age": 3, "Emoji": 🥉},
-    {"Name": "Name4", "Age": 4, "Emoji": 👍},
-    {"Name": "Name5", "Age": 5, "Emoji": 👎},
+    {"Name": "John", "Age": 1, "Emoji": "🥇"},
+    {"Name": "George", "Age": 2, "Emoji": "🥈"},
+    {"Name": "Tommy", "Age": 3, "Emoji": "🥉"},
+    {"Name": "Tammy", "Age": 4, "Emoji": "👍"},
+    {"Name": "Edgar", "Age": 5, "Emoji": "👎"},
 ];
+
+
 
 let outputString = ' ';
 for (const i in objectArray){
-    //console.log(`${i}: ${objectArray[i]}`);
-    //console.log(`Name: ${objectArray[i]}, Age: ${}`);
-    console.log(`${JSON.stringify(objectArray[i])}`);
-    outputString += `${JSON.stringify(objectArray[i])}` + '<br>';
+
+    console.log(`Name: ${objectArray[i].Name} ${objectArray[i].Emoji} Age: ${objectArray[i].Age} <br>`);
+    outputString += `Name: ${objectArray[i].Name} ${objectArray[i].Emoji} Age: ${objectArray[i].Age} <br>`;
 }
-console.log(outputString);
-let test = "hello"
-document.getElementById('output').innerHTML = test;
+document.getElementById('output').innerHTML = outputString;
