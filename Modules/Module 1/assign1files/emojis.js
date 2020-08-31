5533bb74f4feb0c914761422b40b5e3c6e28a5d5
Bryce <bryce.rankin@gmail.com>
2020-08-30 20:33:21 -0700

    Assignment 1 - Code created and testing

--- Modules/Module 1/assign1files/emojis.js
+++ Modules/Module 1/assign1files/emojis.js
@@ -1,2 +1,20 @@
 'use strict'
  
+let objectArray = [
+    {"Name": "Name1", "Age": 1, "Emoji": 🥇},
+    {"Name": "Name2", "Age": 2, "Emoji": 🥈},
+    {"Name": "Name3", "Age": 3, "Emoji": 🥉},
+    {"Name": "Name4", "Age": 4, "Emoji": 👍},
+    {"Name": "Name5", "Age": 5, "Emoji": 👎},
+];
+
+let outputString = ' ';
+for (const i in objectArray){
+    //console.log(`${i}: ${objectArray[i]}`);
+    //console.log(`Name: ${objectArray[i]}, Age: ${}`);
+    console.log(`${JSON.stringify(objectArray[i])}`);
+    outputString += `${JSON.stringify(objectArray[i])}` + '<br>';
+}
+console.log(outputString);
+let test = "hello"
+document.getElementById('output').innerHTML = test;
